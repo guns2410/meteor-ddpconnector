3ca26a4e1c78a26f1cc170d31edc130587729d56
Guns <guns2410@users.noreply.github.com>
2016-02-15 23:55:52 +0530

    Create package.js

---
+++ package.js
@@ -0,0 +1,22 @@
+Package.describe({
+	name         : 'gunjansoni:ddpconnector',
+	version      : '0.0.1',
+	summary      : 'Connect Meteor Servers over DDP Connection (Works on Galaxy)',
+	git          : 'https://github.com/guns2410/meteor-ddpconnector',
+	documentation: 'README.md'
+});
+
+Package.onUse(function (api) {
+	api.versionsFrom('1.0.0');
+	api.use('ecmascript');
+	api.use('ddp');
+	api.addFiles('ddpconnector.js');
+	api.export("DDPConnector");
+});
+
+Package.onTest(function (api) {
+	api.use('ecmascript');
+	api.use('tinytest');
+	api.use('gunjansoni:ddpconnector');
+	api.addFiles('ddpconnector-tests.js');
+});
